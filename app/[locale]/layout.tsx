31493f6b0834f99e5801c6186fd2945971d4e827
Mindia Arabuli <mindiaarabuli4@gmail.com>
2026-06-04 17:13:06 +0400

    fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,9 +2,6 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import { routing } from "@/features/i18n/routing";
 import { notFound } from "next/navigation";
-import Header from "@/features/common/components/composites/Header";
-import { ResponsiveContainer } from "@/components/ResponsiveContainer";
-import Footer from "@/features/common/components/composites/Footer";
 
 export default async function LocaleLayout({
   children,
@@ -33,11 +30,7 @@ export default async function LocaleLayout({
   return (
     <NextIntlClientProvider>
       <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">
-          <ResponsiveContainer>{children}</ResponsiveContainer>
-        </main>
-        <Footer />
+        <main className="flex-1">{children}</main>
       </div>
     </NextIntlClientProvider>
   );
