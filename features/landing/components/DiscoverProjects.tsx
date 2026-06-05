6f364e3d58feaf9be8341e9e622a5ccf2ab4be57
N1k0l0zI0b1dze <nio63106@gmail.com>
2026-06-05 14:16:59 +0400

    Set DiscoverProjects as homepage
    Replace the Landing page with the new DiscoverProjects component in app/[locale]/page.tsx and render it centered. Flesh out DiscoverProjects with layout, Card subcomponent and horizontal scroll behavior. Remove several unused landing components (AboutUs, Footer, Landing, NewProject, Partners, Statistic) to clean up the feature. Update globals.css to normalize quote style for imports/font-face and add .scrollbar-hide utilities. Add tailwind-scrollbar-hide to package.json (and package-lock) to support the scrollbar hiding.

--- features/landing/components/DiscoverProjects.tsx
+++ features/landing/components/DiscoverProjects.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
 const DiscoverProjects = () => {
-  return <div>Hello this is discover projects</div>;
+  return (
+    <div className="flex justify-center items-center w-97.5 h-100 md:w-208.5 md:h-95 lg:w-full lg:h-130 mt-18 lg:mt-14 px-4.5 md:px-[24.5px] lg:px-30 bg-red-500">
+      {/* content container */}
+      <div className="flex flex-col items-center w-full h-full bg-gray-500">
+        {/* name */}
+        <div className="w-full h-12 md:h-12 lg:h-14.5 bg-purple-500"></div>
+        {/* Cards */}
+        <div className="flex flex-row w-full max-w-300 h-72 overflow-x-auto md:overflow-visible scrollbar-hide md:h-65 lg:h-90 mt-4 md:mt-6 lg:mt-10 gap-4 md:gap-5.5 lg:gap-6 bg-black">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        {/* View All Courses */}
+        <div className="w-58.5 h-6 md:w-71.25 lg:w-83.25 mt-6 lg:mt-10 bg-black"></div>
+      </div>
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className="w-70.25 h-full md:w-60.75 md:h-63 lg:w-96 lg:h-90 rounded-2xl shrink-0 bg-white"></div>
+  );
 };
 
 export default DiscoverProjects;
