22e1f8dbf5aa2bb35dfdbeb9a5ce569c5722f030
Mindia Arabuli <mindiaarabuli4@gmail.com>
2026-06-11 14:43:24 +0400

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -31,9 +31,8 @@ export default async function LocaleLayout({
   return (
     <NextIntlClientProvider>
       <div className="flex flex-col min-h-screen">
-        <main className="flex-1">{children}
-        </main>
-          <Footer />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </div>
     </NextIntlClientProvider>
   );
