4dd717a6783287d5686d1b31fd1730982f9f0fa7
tsiklaurii <tsiklaurinino321@gmail.com>
2026-06-13 21:19:29 +0400

    fix/header
    adjust logo size for small screen sizes

--- common/components/Header.tsx
+++ common/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="w-full flex justify-center">
             <div className="flex items-center w-full min-w-0 max-w-300 p-[20px_24px] lg:h-37 h-30 font-mecomisce lg:text-[24px] text-[20px] leading-6 justify-between">
-                <Image alt="Logo" src="/images/header/Logo.svg" width={158.63} height={100} className="cursor-pointer lg:w-[158.63px] w-31.25 lg:h-25 h-20" />
+                <Image alt="Logo" src="/images/header/Logo.svg" width={158.63} height={100} className="cursor-pointer lg:w-[158.63px] md:w-31.25 w-[101.52px] lg:h-25 md:h-20 h-16" />
                 <div className="flex gap-16 items-center">
                     <nav className="hidden md:block">
                         <ul className="flex lg:gap-12 gap-4">
